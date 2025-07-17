a25c742ebe4c7fce8c525b5fa412b066756883c3
anjarmath <anjarmath@gmail.com>
2025-07-17 19:03:22 +0700

    fix: set action to no store

--- src/features/landing/actions.ts
+++ src/features/landing/actions.ts
@@ -1,12 +1,15 @@
 "use server";
 
 import { db } from "@/server/db";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function getProfile() {
+  noStore();
   return db.profile.findFirst();
 }
 
 export async function getPortfolios() {
+  noStore();
   return db.portfolio.findMany({
     where: {
       visible: true,
@@ -15,6 +18,7 @@ export async function getPortfolios() {
 }
 
 export async function getExperiences() {
+  noStore();
   return db.experience.findMany({
     orderBy: {
       index: "asc",
@@ -23,5 +27,6 @@ export async function getExperiences() {
 }
 
 export async function getCertificates() {
+  noStore();
   return db.certificate.findMany();
 }
